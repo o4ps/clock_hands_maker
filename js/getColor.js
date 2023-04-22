e603860d31f3373639d08a1de906df91fe481161
toy101 <toy101dev@gmail.com>
2023-04-22 15:13:17 +0900

    fix: github ioでrepo名をurlに含めるように修正

--- js/getColor.js
+++ js/getColor.js
@@ -1,20 +1,20 @@
-function butotnClick() {
-  var currentUrl = window.location.href;
-  var domain = window.location.origin;
+function changeColor() {
   var firstColor = colorBox1.value.replace("#", "%23");
   var secondColor = colorBox2.value.replace("#", "%23");
-  // alert(
-  //   "現在の URL: " +
-  //     currentUrl +
-  //     "\nドメイン: " +
-  //     domain +
-  //     "現在選択している色は " +
-  //     firstColor +
-  //     " です"
-  // );
-  // console.log("現在選択している色は " + colorBox.value + " です");
+
+  // URLオブジェクトを作成
+  const urlObject = new URL(window.location.href);
+  // URLのsearchプロパティを空文字列に設定して、パラメータを削除
+  urlObject.search = "";
+  // 新しいURL文字列を生成して返す
+  var currentUrlWithOutParam = urlObject.toString();
+
   location.href =
-    domain + "?firstColor=" + firstColor + "&secondColor=" + secondColor;
+    currentUrlWithOutParam +
+    "?firstColor=" +
+    firstColor +
+    "&secondColor=" +
+    secondColor;
 }
 
 let colorBox1 = document.getElementById("colorBox1");
@@ -23,4 +23,4 @@ let colorBox2 = document.getElementById("colorBox2");
 colorBox2.value = "#F0F0F0";
 
 let checkButton = document.getElementById("checkButton");
-checkButton.addEventListener("click", butotnClick);
+checkButton.addEventListener("click", changeColor);
