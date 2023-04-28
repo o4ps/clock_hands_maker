47c2bef94344b76efbe1772d10ea05d4afa384d2
toy101 <toy101dev@gmail.com>
2023-04-28 14:53:04 +0900

    jsのファイル名と機能が逆だったので修正

--- js/changeColor.js
+++ js/changeColor.js
@@ -1,65 +1,26 @@
-function getColorByParam(name, url) {
-  if (!url) url = window.location.href;
-  name = name.replace(/[\[\]]/g, "\\$&");
-  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-    results = regex.exec(url);
-  if (!results) return null;
-  if (!results[2]) return "";
-  return decodeURIComponent(results[2].replace(/\+/g, " "));
+function changeColor() {
+  var firstColor = colorBox1.value.replace("#", "%23");
+  var secondColor = colorBox2.value.replace("#", "%23");
+
+  // URLオブジェクトを作成
+  const urlObject = new URL(window.location.href);
+  // URLのsearchプロパティを空文字列に設定して、パラメータを削除
+  urlObject.search = "";
+  // 新しいURL文字列を生成して返す
+  var currentUrlWithOutParam = urlObject.toString();
+
+  location.href =
+    currentUrlWithOutParam +
+    "?firstColor=" +
+    firstColor +
+    "&secondColor=" +
+    secondColor;
 }
 
-function isValidColor(color) {
-  if (!color || color.length === 0) {
-    return false;
-  }
+let colorBox1 = document.getElementById("colorBox1");
+colorBox1.value = "#F0F0F0";
+let colorBox2 = document.getElementById("colorBox2");
+colorBox2.value = "#F0F0F0";
 
-  var colorTestElement = document.createElement("div");
-  colorTestElement.style.color = color;
-  return colorTestElement.style.color.length > 0;
-}
-
-function changeCSS() {
-  var firstColorParam = getColorByParam("firstColor");
-  var secondColorParam = getColorByParam("secondColor");
-  var element = document.createElement("style");
-  document.head.appendChild(element);
-  var sheet = element.sheet;
-
-  if (isValidColor(firstColorParam)) {
-    sheet.insertRule(
-      ".clock .circle#sc i { background: " + firstColorParam + "; }",
-      0
-    );
-    sheet.insertRule(
-      ".clock .circle#sc i { box-shadow: 0 70px 0 " + firstColorParam + "; }",
-      0
-    );
-    sheet.insertRule(
-      ".clock::before { box-shadow: 0 0 0 4px " +
-        firstColorParam +
-        ", 0 0 0 6px " +
-        firstColorParam +
-        "; }",
-      0
-    );
-  }
-
-  if (isValidColor(secondColorParam)) {
-    sheet.insertRule(
-      ".clock .circle#hr i { box-shadow: 0 70px 0 " + secondColorParam + "; }",
-      0
-    );
-    sheet.insertRule(
-      ".clock .circle#mn i { box-shadow: 0 70px 0 " + secondColorParam + "; }",
-      0
-    );
-    sheet.insertRule(
-      ".clock .circle i { background:" + secondColorParam + "; }",
-      0
-    );
-    sheet.insertRule(
-      ".clock::before { background:" + secondColorParam + "; }",
-      0
-    );
-  }
-}
+let checkButton = document.getElementById("checkButton");
+checkButton.addEventListener("click", changeColor);
